86d4d9ab2ea01562b662d5a8ae08e69e7f458574
SherpaMilan <msherpa574@icloud.com>
2024-02-14 15:52:22 +1100

    projects and footer section done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,31 @@ import React from "react";
 export const Footer = () => {
   const siteMap = {
     color: "rgba(115,169,107,0.94)",
+    fontSize: "1rem",
+    fontWeight: "bold",
   };
   const locationStyle = {
     color: "rgba(115,169,107,0.94)",
+    fontWeight: "bold",
+    fontSize: "1rem",
   };
   const hourStyle = {
     color: "rgba(115,169,107,0.94)",
+    fontWeight: "bold",
+    fontSize: "1rem",
   };
   return (
     <footer>
       <div className="footer-container flex">
         <div className="footer-top flex">
-          <div>
-            <h3 className="text-xl  font-bold mb-3">
+          <div className="sitemap">
+            <h1 className="text-xl  font-bold mb-3">
               SiteMap
               <i
                 class="bx bx-sitemap bx-flashing bx-flip-horizontal"
                 style={siteMap}
               ></i>
-            </h3>
+            </h1>
             <ul className="flex">
               <li>
                 <a href="#home">Home</a>
@@ -40,34 +46,39 @@ export const Footer = () => {
               </li>
             </ul>
           </div>
+
           {/* location  */}
-          <div className="mb-6 mr-4 md:mb-0">
-            <h3 className="text-xl font-bold mb-3">
+          <div className="mb-6 mr-4 md:mb-0 location">
+            <h1 className=" text-2xl text-blue-500 font-bold mb-4">
               Location
               <i
                 class="bx bx-current-location bx-flashing bx-flip-horizontal"
                 style={locationStyle}
               ></i>
-            </h3>
-            <p className="text-black mb-2">789 Repository Road</p>
-            <p className="text-black mb-2">Commitville, Branch State</p>
-            <p className="text-black">Zip Code: 45678</p>
+            </h1>
+            <ul className="flex">
+              <li className="text-black mb-2">789 Repository Road</li>
+              <li className="text-black mb-2">Commitville, Branch State</li>
+              <li className="text-black">Zip Code: 45678</li>
+            </ul>
           </div>
 
-          <div className="mb-6  md:mb-0">
+          <div className="mb-6  md:mb-0 ophours">
             <h3 className="text-xl  font-bold mb-3">
               Opening Hours
               <i class="bx bxs-hourglass" style={hourStyle}></i>
             </h3>
-            <p className="text-black mb-2 shadow-sm">
-              Monday to Friday: 9 AM - 5 PM
-            </p>
-            <p className="text-black mb-2 shadow-sm">
-              Weekends: Coding marathons, please disturb responsibly
-            </p>
-            <p className="text-black shadow-sm">
-              Holidays: Debugging in progress, expect delays
-            </p>
+            <ul className="flex">
+              <p className="text-black mb-2 shadow-sm">
+                Monday to Friday: 9 AM - 5 PM
+              </p>
+              <p className="text-black mb-2 shadow-sm">
+                Weekends: Coding marathons, please disturb responsibly
+              </p>
+              <p className="text-black shadow-sm">
+                Holidays: Debugging in progress, expect delays
+              </p>
+            </ul>
           </div>
         </div>
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,47 +1,69 @@
 import React from "react";
 import camera from "../assets/camera.jpg";
-import camera1 from "../assets/camera1.jpg";
-import camera2 from "../assets/camera2.jpg";
+import littlelemon from "../assets/lemon.jpg";
+import library from "../assets/library.jpg";
+import movie from "../assets/movie.jpg";
+import calculator from "../assets/calculator.jpg";
+import store from "../assets/store.jpg";
+const projects = [
+  {
+    image: camera,
+    description: "Expense Tracker App",
+    link: "https://mpa-money-tracker-app.vercel.app/",
+  },
+  {
+    image: littlelemon,
+    description: "Little Lemon Restaurant",
+    link: "https://littlelemon-capstone-project.vercel.app/",
+  },
+
+  {
+    image: library,
+    description: "Digital Library",
+    link: "https://milan-digital-library.vercel.app/",
+  },
+  {
+    image: store,
+    description: "E-Commerce Store",
+    link: "https://online-shopping-six-alpha.vercel.app/",
+  },
+  {
+    image: movie,
+    description: "Movie App collection",
+    link: "https://my-movie-app-cyan.vercel.app/",
+  },
+  {
+    image: calculator,
+    description: "Calculator",
+    link: " https://calculatorapp-jet.vercel.app/",
+  },
+];
 
 const Projects = () => {
   return (
-    <section className="section4 flex" id="projects">
-      <h1 className="title">Projects</h1>
+    <>
+      <section className="section4 flex" id="projects">
+        <h1 className="title">Projects</h1>
 
-      <div className="projects-container grid">
-        <div className="project-card flex">
-          <a href="https://mpa-money-tracker-app.vercel.app/">
-            <div className="top">
-              <img src={camera} alt="" />
+        <div className="projects-container grid">
+          {projects.map((project, index) => (
+            <div className="project-card flex" key={index}>
+              <a href={project.link}>
+                <div className="top">
+                  <img src={project.image} alt="" />
+                </div>
+              </a>
+
+              <div className="botton">
+                <a href={project.link} className="explore-button">
+                  <p> 🚀 Explore: {project.description}</p>
+                </a>
+              </div>
             </div>
-          </a>
-          <div className="bottom">
-            <p>Expense Tracker App</p>
-            <p></p>
-          </div>
+          ))}
         </div>
-
-        <div className="project-card flex">
-          <div className="top">
-            <img src={camera1} alt="" />
-          </div>
-          <div className="bottom">
-            <p>December 10, 2022</p>
-            <p>Parallax Demo</p>
-          </div>
-        </div>
-
-        <div className="project-card flex">
-          <div className="top">
-            <img src={camera2} alt="" />
-          </div>
-          <div className="bottom">
-            <p>January 1, 2023</p>
-            <p>Animation and Position</p>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
